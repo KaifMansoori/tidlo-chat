c83e4befdbcb2a39037409d2184a0f3bc0932156
Kaif Mohd <corewithkaif@corewithkaif.local>
2026-05-17 13:51:27 +0530

    Bypass CORS typo issues by reflecting origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
